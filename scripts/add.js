d789ea194af7208b6dca6cce1d0a7db9ed16871e
PikaKight <tmmarcus73@gmail.com>
2022-09-13 11:36:05 -0400

    continuing to write the adding code

--- scripts/add.js
+++ scripts/add.js
@@ -1,19 +1,33 @@
-const fs = require('fs')
+const fs = require('fs');
 
-let pkmnNum = document.getElementById("pkmnNum")
-let pkmnName = document.getElementById("pkmnName")
-let gen = document.getElementById("gen")
-let preEvol = document.getElementById("preEvol")
-let evol = document.getElementById("evol")
-let desc = document.getElementById("desc")
+// let pkmnNum = document.getElementById("pkmnNum");
+// let pkmnName = document.getElementById("pkmnName");
+// let gen = document.getElementById("gen");
+// let preEvol = document.getElementById("preEvol");
+// let evol = document.getElementById("evol");
+// let desc = document.getElementById("desc");
+
+// let pkmn = {
+//     "name": pkmnNum,
+//     "generation": pkmnName,
+//     "preEvol": gen,
+//     "evol": preEvol,
+//     "Type1": evol,
+//     "Type2": desc
+// }
 
 let pkmn = {
-    "name": pkmnNum,
-    "generation": pkmnName,
-    "preEvol": gen,
-    "evol": preEvol,
-    "Type1": evol,
-    "Type2": desc
+    "name": "pkmnNum",
+    "generation": "pkmnName",
+    "preEvol": "gen",
+    "evol": "preEvol",
+    "Type1": "evol",
+    "Type2": "desc"
 }
 
-let pkmnData = fs.readFileSync('../Resources/pokmn-data/pokmn-data.json', 'utf-8')
+let pkmnData = fs.readFileSync('Resources/pkmn-data/pkmn-data.json', 'utf-8');
+
+let pkmns = JSON.parse(pkmnData)
+
+
+console.log(pkmnData);
