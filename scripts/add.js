3eeba565213861b468b6c4fc3179dad575e22b0e
Marcus Tuen Muk <tmmarcus73@gmail.com>
2022-09-13 10:22:16 -0400

    finished a page to add new pokemon onto the pokédex and started writing the code for adding it into the json

--- scripts/add.js
+++ scripts/add.js
@@ -1,2 +1,19 @@
 const fs = require('fs')
 
+let pkmnNum = document.getElementById("pkmnNum")
+let pkmnName = document.getElementById("pkmnName")
+let gen = document.getElementById("gen")
+let preEvol = document.getElementById("preEvol")
+let evol = document.getElementById("evol")
+let desc = document.getElementById("desc")
+
+let pkmn = {
+    "name": pkmnNum,
+    "generation": pkmnName,
+    "preEvol": gen,
+    "evol": preEvol,
+    "Type1": evol,
+    "Type2": desc
+}
+
+let pkmnData = fs.readFileSync('../Resources/pokmn-data/pokmn-data.json', 'utf-8')
