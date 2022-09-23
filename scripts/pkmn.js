51108f9942ce582e364cf8be7f80f99781d3a7e6
Marcus Tuen Muk <tmmarcus73@gmail.com>
2022-09-23 14:07:16 -0400

    changed some css

--- scripts/pkmn.js
+++ scripts/pkmn.js
@@ -15,16 +15,21 @@ fetch("../Resources/pkmn-data/pkmnData.json")
         
         let img = document.createElement('img');
         img.src = `${pkmn.img}`
+        img.setAttribute("class", "pics")
         pkmnImg.appendChild(img)
 
         pkmnBox.appendChild(pkmnImg)
-
+        
         let pkmnInfor = document.createElement('dev');
         pkmnInfor.setAttribute("class", "pkmnInfor");
+        
         let info = document.createElement('span');
         info.innerHTML = pkmn.name
         pkmnInfor.appendChild(info)
-
+        
+        let btm = document.createElement('button');
+        btm.setAttribute("id", (pkmn.num%10) )
+        pkmnInfor.appendChild(btm)
 
         pkmnBox.appendChild(pkmnInfor)
 
